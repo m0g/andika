cea8012363fbb448681dbe70c20fc503b270adee
Loïc Nogues <nogues.loic@gmail.com>
2014-05-19 13:46:34 +0300

    window name takes current file name

--- app/front/index.js
+++ app/front/index.js
@@ -5,15 +5,15 @@
 
   window.onload = function() {
     var writer = new Writer()
+      , title = document.getElementsByTagName('title')[0]
       , editor = document.getElementById('editor');
 
     ipc.sendChannel('window-loaded', true);
 
     ipc.on('open-file', function(filePath) {
-      //alert('incoming file: ' + filePath);
 
       writer.openFile(filePath, function(data) {
-        //alert('File received!');
+        title.innerHTML = 'Andika - ' + filePath;
         editor.innerHTML = data;
       });
     });
